96304386de5dbf763fab802c9a46b356f37dd10d
Andreas J. P <supercigar@gmail.com>
2018-12-01 18:33:29 +0100

    Day 1 done. EZ Clap

---
+++ day1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  1 17:19:58 2018
+
+@author: Supercigar
+"""
+
+filepath = "day1_input.txt"
+
+# PART ONE
+def partOne(freq):
+    with open(filepath) as file:
+        for row in file:
+            freq += eval(row)
+    return freq
+
+
+# PART TWO
+def partTwo(freq):
+    seen = set([freq])
+    with open(filepath) as file:
+        for row in file:
+            freq += eval(row)
+            if freq in seen:
+                return freq
+    return freq
