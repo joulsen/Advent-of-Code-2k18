e5302c8d5eddd83c86a4e75fd6b705fe60a327da
Andreas J. P <supercigar@gmail.com>
2019-08-08 17:48:56 +0200

    Rewrote and completed part 2.

--- day08.py
+++ day08.py
@@ -1,32 +1,41 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 15 15:17:33 2018
+Created on  Dec 15 15:17:33 2018
+Modified on Aug 08 15:56:16 2019
 
 @author: Supercigar
 """
-from anytree import Node, RenderTree
-import pdb
+
+from anytree import Node, findall
+
 data = [int(i) for i in open("day08_input.txt", "r").read().split()]
 
-# PART ONE
-def partOne(parent = None, first = True):
-    if first:
-        global mdsum, ce
-        mdsum = ce = 0
-    cN_children = data[ce]
-    cN_metanums = data[ce+1]
-    ce += 2
-    cN = Node("%d|%d" % (cN_children, cN_metanums), parent=parent)
-    for i in range(cN_children):
-        child = partOne(cN, False)
-    for number in range(cN_metanums):
-        mdsum += data[ce]
-        ce += 1
-    return cN
 
-# PART TWO
+def makeTree(data, i=0, parent=None):
+    node = Node(0, parent=parent)
+    nMeta = data[i+1]
+    nChild = data[i]
+    for c in range(nChild):
+        i = makeTree(data, i=i+2, parent=node)
+    for m in range(nMeta):
+        if not nChild:
+            node.name += data[2+i+m]
+        else:
+            try:
+                node.name += node.children[data[2+i+m]-1].name
+            except IndexError:
+                pass
+    for m in range(nMeta): # Doing 2 for-loops to prevent finding new meta-nodes in previous
+        Node(data[2+i+m], parent=node)
+    if node.is_root: return node
+    else:            return i+nMeta
+
+
+def sumMeta(tree):
+    return sum(node.name for node in findall(tree, lambda node: len(node.children) == 0))
 
 
 # RESULTS
-cN = partOne()
-print("Answer to part one is: %d" % mdsum)
+tree = makeTree(data)
+print("Part one: {0}".format(sumMeta(tree)))
+print("Part two: {0}".format(tree.name))
